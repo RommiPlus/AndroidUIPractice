50fb14fbe4ac27dbe699a9a871fed3851cd6cf84
RommiPlus <junqingt@gmail.com>
2018-07-06 16:34:17 +0800

    提取部分函数

--- app/src/main/java/com/example/tttao/calculator/MainActivity.java
+++ app/src/main/java/com/example/tttao/calculator/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.tttao.calculator;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Html;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -25,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String EQUAL = "=";
     public static final String ADD = "+";
     public static final String MINUS = "-";
-    public static final String MULITY = "x";
+    public static final String MULTIPLY = "x";
     public static final String DIVIDE = "/";
 
     private EditText editText = null;//输入框
@@ -36,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
     //按钮中的字符
     private final String[] buttons = new String[]{
-            "9", "8", "7", "+",
-            "6", "5", "4", "-",
-            "3", "2", "1", "x",
-            "0", "clear", EQUAL, "/",
+            "9", "8", "7", ADD,
+            "6", "5", "4", MINUS,
+            "3", "2", "1", MULTIPLY,
+            "0", CLEAR, EQUAL, DIVIDE,
     };
 
     @Override
@@ -75,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
     private void ExecuteExpression() {
         String result = null;
         try {
-            result = convertExpr(toDoString);
+            result = calculatePost(convertToPostfixExpression(toDoString));
         } catch (Exception e) {
             isExecuteNow = false;
         }
@@ -92,88 +91,73 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param expression
      */
-    private String convertExpr(String expression) {
-        int InfixIndex = 0;
-        int PostfixIndex = 0;
-        String PostStr[] = new String[expression.length()];
-        PostStr[0] = "";
-        Stack<String> stack = new Stack<String>();
-        //char post[] = new char[100];
-        String topString;//栈顶元素
-        String curString = "";//当前元素
-        String NextString = "";//当前元素的下一个元素
+    private String[] convertToPostfixExpression(String expression) {
+        int infixIndex = 0;
+        int postfixIndex = 0;
+        String postfix[] = new String[expression.length()];
+        postfix[0] = "";
+        Stack<String> stack = new Stack<>();
+        String top;//栈顶元素
+        String current;//当前元素
+        String next = "";//当前元素的下一个元素
         int isMul = 0;//判断是否是复数
-        while (!NextString.equals(EQUAL)) {
-            curString = String.valueOf(expression.charAt(InfixIndex));
-            NextString = String.valueOf(expression.charAt(InfixIndex + 1));
-            //  如果是数字，直接添加进字符中
-            // 如果前下一个字符也是数字，把他们并到一起
-            if (curString.matches("[\\d\\.]") && NextString.matches("[\\d\\.]")) {
+        while (!next.equals(EQUAL)) {
+            current = String.valueOf(expression.charAt(infixIndex));
+            next = String.valueOf(expression.charAt(infixIndex + 1));
+            if (isMatchesNumber(current) && isMatchesNumber(next)) {
+                // 如果是数字，直接添加进字符中
+                // 如果前下一个字符也是数字，把他们并到一起
                 if (isMul == 0) {
-                    PostStr[PostfixIndex] = PostStr[PostfixIndex] + curString.concat(NextString);
-                    Log.i("a", "PostStr[i]" + PostStr[PostfixIndex]);
+                    postfix[postfixIndex] = postfix[postfixIndex] + current.concat(next);
                 } else {
-                    PostStr[PostfixIndex] = PostStr[PostfixIndex].concat(NextString);
-                    Log.i("a", "PostStr[i]" + PostStr[PostfixIndex]);
+                    postfix[postfixIndex] = postfix[postfixIndex].concat(next);
                 }
                 isMul = 1;
-                //InfixIndex++;
-                //PostfixIndex++;
-            } else if (curString.matches("[\\d\\.]") && NextString.matches("[^\\d\\.]") && isMul == 0) {
-                PostStr[PostfixIndex] = curString;
-                Log.i("a", "PostStr[i]" + PostStr[PostfixIndex]);
-                PostfixIndex++;
-                PostStr[PostfixIndex] = "";
-            } else if (curString.matches("[\\d\\.]") && NextString.matches("[^\\d\\.]") && isMul == 1) {
+            } else if (isMatchesNumber(current) && next.matches("[^\\d.]") && isMul == 0) {
+                postfix[postfixIndex] = current;
+                postfixIndex++;
+                postfix[postfixIndex] = "";
+            } else if (isMatchesNumber(current) && next.matches("[^\\d.]") && isMul == 1) {
                 isMul = 0;
-                PostfixIndex++;
-                PostStr[PostfixIndex] = "";
-            } else if (curString.equals(")")) {
-                //如果是右括号，将栈中其他运算符出栈，添加进字符数组，知道匹配到左括号
-                while (!stack.peek().equals("(")) {
-                    PostStr[PostfixIndex] = stack.peek();
-                    PostfixIndex++;
-                    PostStr[PostfixIndex] = "";
-                    stack.pop();
-                }
-                stack.pop();
-            } else if (curString.equals("+") || curString.equals("-")) {
+                postfixIndex++;
+                postfix[postfixIndex] = "";
+            } else if (current.equals(ADD) || current.equals(MINUS)) {
                 //如果是加减符号
-                if (stack.isEmpty())//空栈就直接入栈
-                    stack.push(curString);
-                else {//弹出优先级高于加减的运算符(因为加减运算符优先级最低)
+                if (stack.isEmpty()) {
+                    //空栈就直接入栈
+                    stack.push(current);
+                } else {
+                    //弹出优先级高于加减的运算符(因为加减运算符优先级最低)
                     while (!stack.isEmpty()) {
-                        topString = stack.peek();
+                        top = stack.peek();
                         stack.pop();
-                        if (topString.equals("(")) {
-                            stack.push(topString);
-                            break;
-                        } else {
-                            PostStr[PostfixIndex] = topString;
-                            PostfixIndex++;
-                            PostStr[PostfixIndex] = "";
-                        }
+                        postfix[postfixIndex] = top;
+                        postfixIndex++;
+                        postfix[postfixIndex] = "";
                     }
-                    stack.push(curString);
+                    stack.push(current);
                 }
-            } else if (curString.equals("x") || curString.equals("/") || curString.equals("(")) {
+            } else if (current.equals(MULTIPLY) || current.equals(DIVIDE)) {
                 //如果是乘除、左括号，优先级高，直接入栈
-                stack.push(curString);
+                stack.push(current);
             } else {
-                return "格式错误";
+                return new String[]{"格式错误"};
             }
-            InfixIndex++;
-
-            Log.i("ss", "PostStr[i]" + PostStr[PostfixIndex]);
+            infixIndex++;
         }
+
         //字符遍历完后将栈中剩余的字符出栈
         while (!stack.isEmpty()) {
-            PostStr[PostfixIndex] = stack.peek();
-            PostfixIndex++;
+            postfix[postfixIndex] = stack.peek();
+            postfixIndex++;
             stack.pop();
         }
-        return calculatePost(PostStr);
 
+        return postfix;
+    }
+
+    private boolean isMatchesNumber(String current) {
+        return current.matches("[\\d.]");
     }
 
 
@@ -187,24 +171,28 @@ public class MainActivity extends AppCompatActivity {
         for (String s : post) {
             if (TextUtils.isEmpty(s)) continue;
 
-            //遇到运算符就对栈顶的两个数字运算
-            if (isFourOperatorString(s) && !list.isEmpty()) {
+            // 遇到数字就入栈
+            if (!isFourOperatorString(s) || list.isEmpty()) {
+                list.push(s);
+            } else {
+                // 遇到运算符就对栈顶的两个数字运算
                 double num1 = Double.valueOf(list.pop());
-                double num2 = Double.valueOf(list.pop());
-                if (s.equals(DIVIDE) && num1 == 0) {
+                if (isDivideZero(s, num1)) {
                     return "除数不能为空";
                 }
-                list.push(String.valueOf(calculate(num2, num1, s)));
-            } else {
-                //遇到数字就入栈
-                list.push(s);
+                list.push(String.valueOf(calculate(Double.valueOf(list.pop()), num1, s)));
             }
         }
 
         if (!list.isEmpty()) {
             return list.pop();
         }
+
         return null;
+    }
+
+    private boolean isDivideZero(String s, double divisor) {
+        return s.equals(DIVIDE) && divisor == 0;
     }
 
     private static double calculate(double num2, double num1, String op) {
@@ -213,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
                 return add(num2, num1);
             case MINUS:
                 return minus(num2, num1);
-            case MULITY:
+            case MULTIPLY:
                 return multiply(num2, num1);
             case DIVIDE:
                 return divide(num1, multiply(num2, 1.0));
@@ -279,7 +267,7 @@ public class MainActivity extends AppCompatActivity {
      * @return
      */
     private boolean isFourOperatorString(String text) {
-        return text.equals(ADD) || text.equals(MINUS) || text.equals(MULITY) || text.equals(DIVIDE);
+        return text.equals(ADD) || text.equals(MINUS) || text.equals(MULTIPLY) || text.equals(DIVIDE);
     }
 
 
